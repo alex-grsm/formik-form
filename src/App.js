f85640ce1c1a20f77774c805767ae17568e625f0
Alex Grsm <alexey.grsm@gmail.com>
2023-05-13 14:41:50 +0300

    formik

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './app.scss';
+import CustomForm from './FormikForm';
+
+function App() {
+    return (
+        <div className='app'>
+            <CustomForm />
+        </div>
+    );
+}
+
+export default App;
